d41ff1841a194a9c8c086d295a982bc33f62c882
dave1725 <72213961+dave1725@users.noreply.github.com>
2024-02-14 23:22:13 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily : {
+      display : ["Open Sans","sans-serif"],
+      body : ["Open Sans","sans-serif"],
+    },
+    extend: {
+      screens : {
+        mf : "990px",
+      }
+    },
+  },
+  plugins: [],
+}
